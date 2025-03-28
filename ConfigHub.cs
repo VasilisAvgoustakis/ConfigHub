8043cf9ff7182cf46ab02fa2f97a97c5378eb399
vasi <b.avg@hotmail.com>
2025-03-28 17:20:28 +0100

    Implmenting class methods

---
+++ ConfigHub.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+using System.Text.Json;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace ConfigManagement
+{
+  //Double check lock and lazy initialization for safe multithreading
+  class ConfigHub
+  {
+    // a lock object that will be used to synchronize threads during first access to the Singleton
+    private static readonly object _lock = new object();
+    
+
+    private static ConfigHub _instance;
+    public static ConfigHub Instance
+    {
+      get
+      {
+        if (_instance == null)
+        {
+          lock (_lock)
+          {
+            if (_instance == null)
+            {
+              _instance = new ConfigHub();
+            }
+          }
+        }
+        return _instance;
+      }
+    }
+
+    private ConfigHub(){} 
+
+    public void LoadFromJson(object serialiazableObject, string filePath)
+    {
+
+    }
+
+    public void LoadFromYaml(string filePath)
+    {
+      
+    }
+
+    public void SaveToJson<T>(T serialiazableObject, string filePath)
+    {
+      string fileName = $"{filePath}/{serialiazableObject.GetType().Name}.json";
+      var options = new JsonSerializerOptions { WriteIndented = true };
+      string jsonString = JsonSerializer.Serialize<T>(serialiazableObject, options);
+      File.WriteAllText(fileName, jsonString);
+    }
+
+    public void SaveToYaml(string filePath)
+    {
+
+    }
+    
+  }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,37 +1,33 @@
-﻿using System;
-using System.Threading;
-using System.Text.Json;
-using YamlDotNet.Serialization;
-using YamlDotNet.Serialization.NamingConventions;
+using System;
+using ConfigManagement;
+using System.Threading.Tasks;
 
-namespace ConfigHub
+
+public class Program
 {
-  //Double check lock and lazy initialization for safe multithreading
-  class ConfigHub
+
+  public class WeatherForecast
   {
-    private ConfigHub(){} 
-
-    private static ConfigHub _instance;
-
-    // a lock object that will be used to synchronize threads during first access to the Singleton
-    private static readonly object _lock = new object();
-
-    public static getInstance()
-    {
-      if (_instance == null)
-      {
-        lock (_lock)
-        {
-          if (_nstance == null)
-          {
-            _instance = new ConfigHub();
-          }
-        }
-      }
-      return _instance;
-    }
-
-
+      public DateTimeOffset Date { get; set; }
+      public int TemperatureCelsius { get; set; }
+      public string? Summary { get; set; }
   }
+    
+    
+  public static void Main()
+  { 
+    ConfigHub configManager = ConfigHub.Instance;
+
+    var WeatherForecast = new WeatherForecast()
+    {
+      Date = DateTime.Parse("2019-08-01"),
+      TemperatureCelsius = 25,
+      Summary = "Hot"
+    };
+
+    configManager.SaveToJson(WeatherForecast, "./temp");
+    
+  }
+
 }
 
